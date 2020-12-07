31c874886510c15d3ed225d862768389afe05d4d
Ricardo Delgado <rdelgado@gns.com.co>
2020-12-07 09:35:00 -0500

    Release for do ping lambda

---
+++ src/main/java/com/konecta/doping/DoPingApplication.java
@@ -0,0 +1,74 @@
+package com.konecta.doping;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoPingApplication {
+
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoPingApplication.class, args);
+
+		handleRequest2();
+
+	}
+
+	public static void handleRequest2() {
+
+
+		try {
+			Socket pingSocket = null;
+			PrintWriter out = null;
+			BufferedReader in = null;
+
+			try {
+				pingSocket = new Socket("************", 000);
+//				pingSocket = new Socket("127.0.0.1", 8080);
+				out = new PrintWriter(pingSocket.getOutputStream(), true);
+				in = new BufferedReader(new InputStreamReader(pingSocket.getInputStream()));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			out.println("ping");
+			System.out.println(in.readLine());
+			out.close();
+			in.close();
+			pingSocket.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void handleRequest() {
+
+		try {
+			String ipAddress = "127.0.0.1";
+			InetAddress inet = InetAddress.getByName(ipAddress);
+
+			System.out.println("Sending Ping Request to " + ipAddress);
+			System.out.println(inet.isReachable(5000) ? "Host is reachable" : "Host is NOT reachable");
+
+			ipAddress = "172.20.73.20";
+			inet = InetAddress.getByName(ipAddress);
+
+			System.out.println("Sending Ping Request to " + ipAddress);
+			System.out.println(inet.isReachable(5000) ? "Host is reachable" : "Host is NOT reachable");
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
